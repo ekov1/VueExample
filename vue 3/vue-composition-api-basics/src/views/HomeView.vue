70bfac9c0145faffff0532d5e4917dd654c9453d
ekov1 <kristian_ekov@abv.bg>
2024-07-14 19:37:14 +0300

    add computed property example

--- vue 3/vue-composition-api-basics/src/views/HomeView.vue
+++ vue 3/vue-composition-api-basics/src/views/HomeView.vue
@@ -1,26 +1,69 @@
 <template>
   <div class="home">
+    <h2>{{ appTitle }}</h2>
+    <h3>{{ counterData.title }}</h3>
     <div>
-      <button @click="decreaseCounter" class="btn">-</button>
-      <span class="counter">{{ counter }}</span>
-      <button @click="increaseCounter" class="btn">+</button>
+      <button @click="decreaseCounter(2)" class="btn">-2</button>
+      <button @click="decreaseCounter(1)" class="btn">-</button>
+      <span class="counter">{{ counterData.counter }}</span>
+      <button @click="increaseCounter(1, $event)" class="btn">+</button>
+      <button @click="increaseCounter(2)" class="btn">+2</button>
+    </div>
+    <p>This Counter is {{ oddOrEven }}</p>
+    <div class="edit">
+      <h4>Edit counter title:</h4>
+      <input type="text" v-model="counterData.title">
     </div>
   </div>
 </template>
 
 <script setup>
-import { ref } from 'vue'
+import { reactive, ref, computed } from 'vue'
 
-const counter = ref(0)
+const appTitle = 'My OK App'
+const counter = ref(0),
+  counterTitle = ref('My Counter')
 
-const increaseCounter = () => {
-  counter.value++;
+const counterData = reactive({
+  counter: 0,
+  title: 'My Counter'
+})
+
+const oddOrEven = computed(()=>{
+  if(counterData.counter % 2 == 0){
+    return 'Even'
+  }
+  return 'Odd'
+})
+
+const increaseCounter = (amount, eventObject) => {
+  console.log(eventObject)
+  counterData.counter += amount
 }
 
-const decreaseCounter = () => {
-  counter.value--;
+const decreaseCounter = amount => {
+  counterData.counter -= amount
 }
 </script>
+
+
+<style>
+.home {
+  text-align: center;
+  padding: 20px;
+}
+
+.btn,
+.counter {
+  font-size: 40px;
+  margin: 10px;
+}
+
+.edit {
+  margin-top: 60px;
+}
+</style>
+
 
 <!-- <script>
 import { ref } from 'vue'
@@ -63,16 +106,3 @@ export default {
   }
 }
 </script> -->
-
-<style>
-.home {
-  text-align: center;
-  padding: 20px;
-}
-
-.btn,
-.counter {
-  font-size: 40px;
-  margin: 10px;
-}
-</style>
