846979d245bb8eb8ac302b10c589a8735aa57f1a
ekov1 <kristian_ekov@abv.bg>
2024-07-15 22:28:53 +0300

    feat:move modal to child component, remove lazy loading from routing

---
+++ vue 3/vue-composition-api-basics/src/components/Modal.vue
@@ -0,0 +1,22 @@
+<template>
+    <teleport to=".modals-container">
+        <div class="modal">
+            <h1>This is a modal</h1>
+            <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Id, soluta cumque vero odit repudiandae quia
+                excepturi illum nesciunt, totam voluptate cum! Eum cumque unde perferendis nobis nesciunt magni adipisci
+                dignissimos?</p>
+            <button>Hide modal</button>
+        </div>
+    </teleport>
+</template>
+
+<style>
+.modal {
+    background: beige;
+    padding: 10px;
+    position: absolute;
+    left: 0;
+    top: 0;
+    z-index: 1;
+}
+</style>
